4e4bb68407b87b0f4077d06158d19cab46efbb82
Andrey Timoshenkov <tav2002@yandex.ru>
2023-11-05 16:21:48 +0300

    feat: default port and db adress added

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,8 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 // const { PORT = 3000 } = process.env;
 // const MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb';
-const { PORT, MONGO_URL } = process.env;
+// prettier-ignore
+const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
